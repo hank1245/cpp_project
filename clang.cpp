b537e9951b3c0ab6d7840966297e7f1d231f592a
hank <hank29206880@gmail.com>
2023-11-07 14:01:32 +0900

    iostream

--- clang.cpp
+++ clang.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include <stdio.h>
 
 int main(void) {
-    int point;
-    scanf("%d", &point);
-    printf("point: %d", point);
-    if (true) {
-        }
+    // 섞어서 쓰는것도 되긴 한다
+    for (int i = 0; i < 100; i++) {
+        printf("경희대");
+        std::cout << std::endl;
+    }
 }
