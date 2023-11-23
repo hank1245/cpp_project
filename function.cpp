734d006ec844494cce6a773b2c666a8544a0f184
hank <hank29206880@gmail.com>
2023-11-23 13:53:19 +0900

    gets

--- function.cpp
+++ function.cpp
@@ -1,3 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) { return 0; }
+int main(void) {
+    char Name[10];
+    // gets(Name); 에러 발생 cpp컴파일러를 사용하기 때문
+    // gets_s(Name); 가능
+    // gets_s(Name, sizeof(Name)); 최적
+}
